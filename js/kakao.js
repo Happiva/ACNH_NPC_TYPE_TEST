e5149f01a4b5d4c04e70f65a071b2276f9e66337
Happiva <kiwhhb1016@gmail.com>
2021-06-20 23:02:41 +0900

    결과 페이지 css 수정

--- js/kakao.js
+++ js/kakao.js
@@ -1,2 +1,4 @@
+const API_KEY = env.API_KEY;
+
 Kakao.init(API_KEY);
 Kakao.isInitialized();
--- js/share.js
+++ js/share.js
@@ -1,13 +1,13 @@
-const defUrl = "";
+const defUrl = "https://acnh-npc-test.netlify.app/";
 
 function shareResult() {
-    var resultImg = document.querySelector("#resultImage");
+    var resultImg = document.querySelector('.resultImage');
     var resultAlt = resultImg.firstElementChild.alt;
 
     const title = "동물의 숲 NPC로 알아보는 성격 유형 결과"
-    const shareDesc = dataList[resultList].name;
-    const shareImg = url + 'images/img-' + resultAlt + '.png';
-    const shareURL = url + 'page/result-' + resultAlt + '.html';
+    const shareDesc = resultList[resultAlt].name;
+    const shareImg = defUrl + 'images/img-' + resultAlt + '.png';
+    const shareURL = defUrl + 'page/result-' + resultAlt + '.html';
 
     Kakao.Link.sendDefault({
         objectType: 'feed',
