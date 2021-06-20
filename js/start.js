738d5131b508c82422a104f2d4212070e87557ba
Happiva <kiwhhb1016@gmail.com>
2021-06-21 00:41:47 +0900

    결과 페이지 html 수정

--- js/start.js
+++ js/start.js
@@ -1,8 +1,8 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result")
-const endPoint = 16;
-const select = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+const endPoint = 21;
+const select = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function calculateResult() {
     var result = select.indexOf(Math.max(...select));
